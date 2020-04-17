a10dbc16066ae2b83231ac7edac9c540bb641bd4
ICVRXS <calebhay@rocketmail.com>
2020-04-17 11:45:36 -0700

    added for loop and required characters

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", );
+
 
 var passLength = 0;
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var special = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", ",", "<", ".", ">", "/", "?", "'"];
+    lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    special = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", ",", "<", ".", ">", "/", "?", "'"];
 
 function isPassLengthTrue(passLength) {
   return passLength >= 8 && passLength <= 128;
@@ -23,43 +24,58 @@ var options = {
   "special": confirm ("Would you like special characters in your password?"),
 };
 
-//Starting while loop in case all options are returned as false
-// while (! options.lowercase && ! options.uppercase && ! options.numbers && ! options.special) {
-  
-// }
 
 console.log(options);
 
 var possibleChars = [];
+    requiredChars = [];
+    password = "";
+
 
 function passGen(trueChars) {
   if (options.lowercase){
     possibleChars = possibleChars.concat(lowercase);
+    requiredChars.push(lowercase [Math.floor(Math.random()*lowercase.length)]);
   }
   if (options.uppercase){
     possibleChars = possibleChars.concat(uppercase);
+    requiredChars.push(uppercase [Math.floor(Math.random()*uppercase.length)]);
   }
   if (options.numbers){
     possibleChars = possibleChars.concat(numbers);
+    requiredChars.push(numbers [Math.floor(Math.random()*numbers.length)]);
   }
   if (options.special){
     possibleChars = possibleChars.concat(special);
+    requiredChars.push(special [Math.floor(Math.random()*special.length)]);
   }
 };
 
 passGen(possibleChars);
+// return password;
 
 console.log(possibleChars);
 
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+for (var i = 0; i < options.passLength; i++) {
+  if (requiredChars[i]){
+    password += requiredChars[i];
+  }else{
+    password += possibleChars [Math.floor(Math.random()*possibleChars.length)];
+  }
 
 }
 
+
+
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
