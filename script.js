18d4c292e5f5efe906fec9eda11e433739ab1513
ICVRXS <calebhay@rocketmail.com>
2020-04-17 16:10:03 -0700

    attempted to enable prompts when button is pressed, options not passing into passGen function

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-var generateBtn = document.querySelector("#generate");
-// generateBtn.addEventListener("click", newPass);
-
-
-var passLength = 0;
-    lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     special = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", ",", "<", ".", ">", "/", "?", "'"];
+    button = document.getElementById("generate");
+    passText = document.getElementById("password").innerHTML=password;
 
-function isPassLengthTrue(passLength) {
-  return passLength >= 8 && passLength <= 128;
-}
+// button.addEventListener("click", newPass);
 
-while (! isPassLengthTrue(passLength)) {
-  passLength = parseInt (prompt("Provide a password length between 8 and 128"));
-}
+// function newPass() {
 
-var options = {
-  "passLength": passLength,
-  "lowercase": confirm ("Would you like lowercase letters in your password?"),
-  "uppercase": confirm ("Would you like UPPERCASE letters in your password?"),
-  "numbers": confirm ("Would you like numbers in your password?"),
-  "special": confirm ("Would you like special characters in your password?"),
-};
-console.log(options);
+  var passLength = 0;
 
+  function isPassLengthTrue(passLength) {
+    return passLength >= 8 && passLength <= 128;
+  }
 
-function passGen() {
+  while (! isPassLengthTrue(passLength)) {
+    passLength = parseInt (prompt("Provide a password length between 8 and 128"));
+  }
+
+  var options = {
+      "passLength": passLength,
+      "lowercase": confirm ("Would you like lowercase letters in your password?"),
+      "uppercase": confirm ("Would you like UPPERCASE letters in your password?"),
+      "numbers": confirm ("Would you like numbers in your password?"),
+      "special": confirm ("Would you like special characters in your password?"),
+  };
+
+  passGen(options);
+
+// };
+
+function passGen(options) {
 
   var possibleChars = [];
       requiredChars = [];
@@ -57,26 +62,8 @@ function passGen() {
     }
   }
 
-  console.log(possibleChars);
   return password;
 
 };
 
-console.log(passGen());
-
-document.getElementById("password").innerHTML=password;
-
-
-
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+console.log(password);
